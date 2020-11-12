1f2c0e775718a1a392035ee02e8962617c2ef4d9
kobysysouvanh <71869057+kobysysouvanh@users.noreply.github.com>
2020-11-12 17:39:21 -0500

    review cart added

---
+++ MomAndPop/src/ReviewCart.java
@@ -0,0 +1,29 @@
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.border.EtchedBorder;
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
+import java.awt.SystemColor;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTable;
+
+public class ReviewCart extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public ReviewCart() {
+		setBackground(SystemColor.control);
+		setLayout(null);
+		setBounds(136, 0, 1108, 671);
+		setVisible(false);
+
+	}
+}
