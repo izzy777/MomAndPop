fc5bbf79e2a3cf1d6762b18820c2ca1222c86a6c
malle <malle@DESKTOP-2Q179VN.hsd1.ga.comcast.net>
2020-11-25 12:49:26 -0500

    Resized toppings buttons, added toppings pictures to file structure

--- MomAndPop/src/Toppings.java
+++ MomAndPop/src/Toppings.java
@@ -12,6 +12,7 @@ import javax.swing.border.LineBorder;
 import java.awt.SystemColor;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 
 public class Toppings extends JPanel {
 	int toppingNum;
@@ -36,41 +37,41 @@ public class Toppings extends JPanel {
 		JLabel lblPA = new JLabel("Pineapple");
 		lblPA.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPA.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblPA.setBorder(new LineBorder(new Color(192, 192, 192), 5, true));
+		lblPA.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		lblPA.setAlignmentY(0.0f);
-		lblPA.setBounds(654, 500, 219, 40);
+		lblPA.setBounds(654, 500, 219, 50);
 		add(lblPA);
 		
 		JLabel lblT = new JLabel("Tomatoes");
 		lblT.setHorizontalAlignment(SwingConstants.CENTER);
 		lblT.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblT.setBorder(new LineBorder(new Color(192, 192, 192), 5, true));
+		lblT.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		lblT.setAlignmentY(0.0f);
-		lblT.setBounds(654, 439, 219, 40);
+		lblT.setBounds(654, 439, 219, 50);
 		add(lblT);
 		
 		JLabel lblM = new JLabel("Mushrooms");
 		lblM.setHorizontalAlignment(SwingConstants.CENTER);
 		lblM.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblM.setBorder(new LineBorder(new Color(192, 192, 192), 5, true));
+		lblM.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		lblM.setAlignmentY(0.0f);
-		lblM.setBounds(654, 378, 219, 40);
+		lblM.setBounds(654, 378, 219, 50);
 		add(lblM);
 		
 		JLabel lblO = new JLabel("Onions");
 		lblO.setHorizontalAlignment(SwingConstants.CENTER);
 		lblO.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblO.setBorder(new LineBorder(new Color(192, 192, 192), 5, true));
+		lblO.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		lblO.setAlignmentY(0.0f);
-		lblO.setBounds(654, 314, 219, 40);
+		lblO.setBounds(654, 314, 219, 50);
 		add(lblO);
 		
 		JLabel lblGP = new JLabel("Green Peppers");
 		lblGP.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGP.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblGP.setBorder(new LineBorder(new Color(192, 192, 192), 5, true));
+		lblGP.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		lblGP.setAlignmentY(0.0f);
-		lblGP.setBounds(654, 249, 219, 40);
+		lblGP.setBounds(654, 249, 219, 50);
 		add(lblGP);
 		
 		JLabel lblTopping = new JLabel("Toppings");
@@ -91,69 +92,61 @@ public class Toppings extends JPanel {
 		lblVeggies.setBounds(654, 198, 219, 48);
 		add(lblVeggies);
 		
-		JLabel lblP = new JLabel("Pepperoni");
-		lblP.setHorizontalAlignment(SwingConstants.CENTER);
-		lblP.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblP.setBorder(new LineBorder(new Color(192, 192, 192), 5, true));
-		lblP.setAlignmentY(0.0f);
-		lblP.setBounds(234, 249, 219, 40);
-		add(lblP);
-		
 		JLabel lblS = new JLabel("Sausage");
 		lblS.setHorizontalAlignment(SwingConstants.CENTER);
 		lblS.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblS.setBorder(new LineBorder(new Color(192, 192, 192), 5, true));
+		lblS.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		lblS.setAlignmentY(0.0f);
-		lblS.setBounds(234, 314, 219, 40);
+		lblS.setBounds(234, 314, 219, 50);
 		add(lblS);
 		
 		JLabel lblH = new JLabel("Ham");
 		lblH.setHorizontalAlignment(SwingConstants.CENTER);
 		lblH.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblH.setBorder(new LineBorder(new Color(192, 192, 192), 5, true));
+		lblH.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		lblH.setAlignmentY(0.0f);
-		lblH.setBounds(234, 378, 219, 40);
+		lblH.setBounds(234, 378, 219, 50);
 		add(lblH);
 		
 		cbP = new JCheckBox("");
 		cbP.setContentAreaFilled(false);
-		cbP.setBounds(200, 249, 21, 40);
+		cbP.setBounds(200, 254, 21, 40);
 		cbP.setFocusable(false);
 		add(cbP);
 		
 		cbS = new JCheckBox("");
 		cbS.setContentAreaFilled(false);
-		cbS.setBounds(200, 314, 28, 40);
+		cbS.setBounds(200, 319, 28, 40);
 		add(cbS);
 		
 		cbH = new JCheckBox("");
 		cbH.setContentAreaFilled(false);
-		cbH.setBounds(200, 378, 28, 40);
+		cbH.setBounds(200, 383, 28, 40);
 		add(cbH);
 		
 		cbGP = new JCheckBox("");
 		cbGP.setContentAreaFilled(false);
-		cbGP.setBounds(620, 249, 28, 40);
+		cbGP.setBounds(620, 254, 28, 40);
 		add(cbGP);
 		
 		cbO = new JCheckBox("");
 		cbO.setContentAreaFilled(false);
-		cbO.setBounds(620, 314, 28, 40);
+		cbO.setBounds(620, 319, 28, 40);
 		add(cbO);
 		
 		cbM = new JCheckBox("");
 		cbM.setContentAreaFilled(false);
-		cbM.setBounds(620, 378, 28, 40);
+		cbM.setBounds(620, 383, 28, 40);
 		add(cbM);
 		
 		cbT = new JCheckBox("");
 		cbT.setContentAreaFilled(false);
-		cbT.setBounds(620, 439, 28, 40);
+		cbT.setBounds(620, 444, 28, 40);
 		add(cbT);
 		
 		cbPA = new JCheckBox("");
 		cbPA.setContentAreaFilled(false);
-		cbPA.setBounds(620, 500, 28, 40);
+		cbPA.setBounds(620, 505, 28, 40);
 		add(cbPA);
 		
 		JLabel lblFirstTopping = new JLabel("First Topping Free");
@@ -162,6 +155,14 @@ public class Toppings extends JPanel {
 		lblFirstTopping.setFont(new Font("Franklin Gothic Demi Cond", Font.PLAIN, 25));
 		lblFirstTopping.setBounds(10, 74, 1088, 130);
 		add(lblFirstTopping);
+		
+		JLabel lblP = new JLabel("Pepperoni");
+		lblP.setHorizontalAlignment(SwingConstants.CENTER);
+		lblP.setFont(new Font("Dialog", Font.PLAIN, 20));
+		lblP.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
+		lblP.setAlignmentY(0.0f);
+		lblP.setBounds(234, 249, 219, 50);
+		add(lblP);
 		
 		
 
